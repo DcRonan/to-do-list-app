e78603570069d2c99ddd6f3ca8a0e609f7a24155
Daniel Ronan <danielconnorronan@gmail.com>
2020-12-21 12:31:15 +0000

    add working functionality to set priority color to to-dos

--- src/js/toDoIndex.js
+++ src/js/toDoIndex.js
@@ -3,7 +3,7 @@ import * as el from './elements';
 const showAndDeleteToDo = () => {
   const showTable = document.createElement('table');
   showTable.setAttribute('id', 'to-do-list-items');
-  showTable.classList = 'w-full'
+  showTable.classList = 'w-full';
   el.toDos.forEach((toDo) => {
     const tableRow = document.createElement('tr');
     const deleteBtn = document.createElement('button');
@@ -37,19 +37,33 @@ const showAndDeleteToDo = () => {
       el.content.appendChild(el.toDoForm);
     });
 
-    tableRow.innerHTML = `<div class="flex justify-between bg-gray-200 p-3 rounded-md w-full">
+    tableRow.innerHTML = `<div class="flex justify-between ${priorityColor(
+      toDo.priority
+    )} p-3 rounded-md w-full">
     <div class="flex items-center"> 
-    <div class="h-4 w-4 mr-3 rounded-sm border border-blue-700"></div>
+    <div class="h-4 w-4 mr-3 rounded-sm border border-blue-400"></div>
     <div>${toDo.title}</div>
     </div>
     <div>${toDo.dueDate}</div>
-  </div>`
+    <div class="info-btn"><i class="fas fa-info-circle"></i></div>
+  </div>`;
+
     tableRow.appendChild(deleteBtn);
     tableRow.appendChild(editBtn);
     showTable.appendChild(tableRow);
   });
 
   return showTable;
+};
+
+const priorityColor = (color) => {
+  if (color === 'Low') {
+    return 'bg-gray-200';
+  } else if (color === 'Medium') {
+    return 'bg-yellow-400';
+  } else {
+    return 'bg-red-700 text-white';
+  }
 };
 
 export default showAndDeleteToDo;
