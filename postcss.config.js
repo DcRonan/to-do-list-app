c8e741e1bfcc901505f3886c63694eb074e28bc5
Daniel Ronan <danielconnorronan@gmail.com>
2020-12-23 13:18:32 +0000

    comment purge-css

--- postcss.config.js
+++ postcss.config.js
@@ -2,12 +2,9 @@ module.exports = {
   plugins: [
     require('tailwindcss')('./tailwind.config.js'),
     require('autoprefixer'),
-     process.env.NODE_ENV === 'production' ? require('@fullhuman/postcss-purgecss')({
-      content: [
-        './src/**/*.js',
-        './dist/index.html',
-      ],
-      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-    }) : null
-  ]
+    // require('@fullhuman/postcss-purgecss')({
+    //   content: ['./src/**/*.js', './dist/index.html'],
+    //   defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+    // }),
+  ],
 };
