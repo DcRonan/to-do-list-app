cfa01d0d578ef0ed73d9db517a3c26f8e9450efd
Daniel Ronan <danielconnorronan@gmail.com>
2020-12-23 11:10:44 +0000

    add purge-css to remove unused css

--- postcss.config.js
+++ postcss.config.js
@@ -2,5 +2,12 @@ module.exports = {
   plugins: [
     require('tailwindcss')('./tailwind.config.js'),
     require('autoprefixer'),
+    require('@fullhuman/postcss-purgecss')({
+      content: [
+        './src/**/*.js',
+        './dist/index.html',
+      ],
+      defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+    })
   ],
 };
