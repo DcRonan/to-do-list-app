89f7b135671801a4a5978be5cacc4b8d56b27193
Daniel Ronan <danielconnorronan@gmail.com>
2020-12-23 13:16:46 +0000

    edit postcss file for prod

--- postcss.config.js
+++ postcss.config.js
@@ -2,12 +2,12 @@ module.exports = {
   plugins: [
     require('tailwindcss')('./tailwind.config.js'),
     require('autoprefixer'),
-    require('@fullhuman/postcss-purgecss')({
+     process.env.NODE_ENV === 'production' ? require('@fullhuman/postcss-purgecss')({
       content: [
         './src/**/*.js',
         './dist/index.html',
       ],
       defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
-    })
-  ],
+    }) : null
+  ]
 };
