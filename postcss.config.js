508b28aed819f67d79ce2ef5074ad5a5d35e356b
Daniel Ronan <danielconnorronan@gmail.com>
2020-12-23 13:20:48 +0000

    optimized bundle file and remove unused css

--- postcss.config.js
+++ postcss.config.js
@@ -2,9 +2,9 @@ module.exports = {
   plugins: [
     require('tailwindcss')('./tailwind.config.js'),
     require('autoprefixer'),
-    // require('@fullhuman/postcss-purgecss')({
-    //   content: ['./src/**/*.js', './dist/index.html'],
-    //   defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
-    // }),
+    require('@fullhuman/postcss-purgecss')({
+      content: ['./src/**/*.js', './dist/index.html'],
+      defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+    }),
   ],
 };
