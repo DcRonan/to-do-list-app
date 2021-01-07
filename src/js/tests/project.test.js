221bcf2b8cbb04e6c80791914704efb805fa6197
Daniel Ronan <danielconnorronan@gmail.com>
2021-01-07 14:53:33 +0000

    add 4 test cases to projects.js

---
+++ src/js/tests/project.test.js
@@ -0,0 +1,20 @@
+import Project from '../projects';
+
+const newProject = new Project('Study');
+const ProjectTwo = new Project();
+
+it('checks if Project is defined', () => {
+  expect(Project).toBeDefined();
+});
+
+it('checks if a new object is an instance of the class Project', () => {
+  expect(newProject).toBeInstanceOf(Project);
+});
+
+it('checks if object value is equal', () => {
+  expect(newProject).toEqual({ title: 'Study' });
+});
+
+it("checks if an object's value is undefined", () => {
+  expect(ProjectTwo).toEqual({ title: undefined });
+});
