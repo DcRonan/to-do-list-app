c6c31f7188c2e94b4ebb0bcaa6a4504f5d718d7f
Daniel Ronan <danielconnorronan@gmail.com>
2021-01-07 15:35:33 +0000

    add test file for ToDoItem and 4 test cases

--- src/js/tests/localStorage.test.js
+++ src/js/tests/localStorage.test.js
@@ -2,21 +2,21 @@ import Project from '../projects';
 
 const newProject = new Project('Study');
 const projectTwo = new Project('Work');
-const arr = [newProject, projectTwo]
-const emptyArr = []
+const arr = [newProject, projectTwo];
+const emptyArr = [];
 
-localStorage.setItem('projects', JSON.stringify(arr))
-localStorage.setItem('selected project', JSON.stringify(newProject))
-localStorage.setItem('empty array', JSON.stringify(emptyArr))
+localStorage.setItem('projects', JSON.stringify(arr));
+localStorage.setItem('selected project', JSON.stringify(newProject));
+localStorage.setItem('empty array', JSON.stringify(emptyArr));
 
 it('checks if object is saved in local storage', () => {
-  expect(localStorage.getItem('projects')).toBe(JSON.stringify(arr))
+  expect(localStorage.getItem('projects')).toBe(JSON.stringify(arr));
 });
 
 it('checks if there is 2 projects saved in local storage', () => {
   expect(JSON.parse(localStorage.getItem('projects')).length).toBe(2);
-})
+});
 
-it ('checks if object in local storage is empty', () => {
-  expect(localStorage.getItem('empty array')).toBe('[]')
-})
+it('checks if object in local storage is empty', () => {
+  expect(localStorage.getItem('empty array')).toBe('[]');
+});
---
+++ src/js/tests/to-do.test.js
@@ -0,0 +1,42 @@
+import ToDoItem from '../toDoItems';
+
+const newToDo = new ToDoItem(
+  'Clean',
+  'the dishes',
+  '10/09/2021',
+  'Medium',
+  'list of dishes',
+  'hoover'
+);
+
+const ToDoTwo = new ToDoItem();
+
+it('checks if ToDoItem is defined', () => {
+  expect(newToDo).toBeDefined();
+});
+
+it('checks if a new object is an instance of the class ToDoItem', () => {
+  expect(newToDo).toBeInstanceOf(ToDoItem);
+});
+
+it('checks if object value is equal', () => {
+  expect(newToDo).toEqual({
+    title: 'Clean',
+    description: 'the dishes',
+    dueDate: '10/09/2021',
+    priority: 'Medium',
+    notes: 'list of dishes',
+    checkList: 'hoover',
+  });
+});
+
+it("checks if an object's value is undefined", () => {
+  expect(ToDoTwo).toEqual({
+    title: undefined,
+    description: undefined,
+    dueDate: undefined,
+    priority: undefined,
+    notes: undefined,
+    checkList: undefined,
+  });
+});
